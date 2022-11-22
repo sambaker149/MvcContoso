07b3a2ad9ffde1739454814f0db4009ca21c206c
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-11-22 17:08:38 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcContoso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
